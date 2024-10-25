a30274d2968fa4410d54ba836adcadad6e75dd7e
GURUDUTT JANGID <63337556+GuruduttJangid97@users.noreply.github.com>
2024-10-25 22:01:04 +0000

    Basics folder Move to Parent

---
+++ Basics/Basics.js
@@ -0,0 +1,10 @@
+const accountId = 123456
+let accountEmail = "KingTchalla@gmail.com"
+var accountPassword = "1232121"
+accountCity = "Ajmer";
+
+accountId = 2
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
