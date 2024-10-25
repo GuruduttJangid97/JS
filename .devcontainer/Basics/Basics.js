3d3036302786e8559acfef878fb752ec9d441d47
GURUDUTT JANGID <63337556+GuruduttJangid97@users.noreply.github.com>
2024-10-25 21:56:55 +0000

    Basic Syntax added

--- .devcontainer/Basics/Basics.js
+++ .devcontainer/Basics/Basics.js
@@ -1,1 +1,10 @@
-console.log("Test Environment");
+const accountId = 123456
+let accountEmail = "KingTchalla@gmail.com"
+var accountPassword = "1232121"
+accountCity = "Ajmer";
+
+accountId = 2
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
