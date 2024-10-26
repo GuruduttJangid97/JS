948f2be68bbee1a077501879db53dac6cb4dbe35
GURUDUTT JANGID <63337556+GuruduttJangid97@users.noreply.github.com>
2024-10-26 05:31:09 +0000

    New File Added

---
+++ Basics/dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // Treat all JS code as newer version
+
+
+// alert(9 + 9) // We are using node, not browser
+
+console.log(9 
+    + 9) // Code Readibility
+
+console.log("Gurudutt Jangid")
