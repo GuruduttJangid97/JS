9e05d170c8b7a514a7a21ff7885d5a645530fc1b
GURUDUTT JANGID <63337556+GuruduttJangid97@users.noreply.github.com>
2024-10-26 08:26:25 +0000

    Comments Added

---
+++ Basics/4_comparison.js
@@ -0,0 +1,25 @@
+console.log(2 > 1);        // Best Conversion
+console.log(2 >= 1);      // Best Conversion
+console.log(2 < 1);      // Best Conversion
+console.log(2 == 1);    // Best Conversion
+console.log(2 != 1);   // Best Conversion
+
+console.log("*************************Break***********************")
+
+console.log("2" > 1) // Produce True
+console.log("02" > 1) // Produce True
+
+console.log("*************************Break***********************")
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); /* True because "The Reason is that an equality check == and comparisons > <>=<= Work Differently Coparsion convert null to a number, Treating it as 0
+Thats why null >= 0 is true and null > 0 is false" */
+
+console.log("*************************Break***********************")
+
+console.log(undefined > 0);    
+console.log(undefined == 0); 
+console.log(undefined >= 0); 
+
+console.log("*************************Break***********************")
