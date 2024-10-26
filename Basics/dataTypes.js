f7066c7ad2ea9d12ba78d0495917093d59a2f5b8
GURUDUTT JANGID <63337556+GuruduttJangid97@users.noreply.github.com>
2024-10-26 05:43:12 +0000

    New Lines

--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -7,3 +7,23 @@ console.log(9
     + 9) // Code Readibility
 
 console.log("Gurudutt Jangid")
+
+let string = "Hi";
+let number = 5;
+let isLoggedIn = false;
+let state = null;
+
+// Number
+// BigInt
+// string => ""
+// boolean => True/False
+// null => Standalon Value
+// Undefined => 
+// Symbol => Unique
+
+// Object
+
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
